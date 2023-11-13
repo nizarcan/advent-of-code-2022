340338cb95e57dab39e0774802d6cedab2570fcd
Nizar Can <nizarcan@yahoo.com>
2023-11-13 23:09:27 +0300

    feat: solve d03a

--- d03a/src/main.rs
+++ d03a/src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("yay day 3");
+use std::{fs, collections::HashMap};
+
+
+fn get_priority(c: char) -> usize {
+    if c.is_ascii_lowercase() {
+        return c as usize - 'a' as usize + 1;
+    } else {
+        return c as usize - 'A' as usize + 27;
+    }
+}
+
+pub fn main() {
+    let file_name = "../data/d03.txt";
+    println!("reading file {file_name:?}");
+
+    let mut priorities = HashMap::new();
+
+    for line in fs::read_to_string(file_name).unwrap().lines() {
+        let (first_half, second_half) = line.split_at(line.len() / 2);
+
+        for c in first_half.chars().filter(|&x| second_half.contains(x)) {
+            let priority = get_priority(c);
+            priorities.entry(c).and_modify(|x| *x += priority).or_insert(priority);
+            break;
+        }
+    }
+
+    let total: usize = priorities.values().sum();
+    println!("total is: {total:?}");
 }
